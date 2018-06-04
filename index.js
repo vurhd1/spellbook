faefde2533d1b2badf824b7708c4f8c7fe359add
Dhruv Ramanujan <dhruv0122@gmail.com>
2018-06-04 19:54:57 -0400

    Added button functionality to change header

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('test')
+const button1 = document.querySelector('button');
+const header1 = document.querySelector('h1');
+
+function changeHeader(){
+    header1.textContent = 'Changed it';
+}
+
+button1.addEventListener('click', changeHeader);
