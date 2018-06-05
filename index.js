1b4fa77b48915e14bdd01041bbc1158e81c14b30
Dhruv Ramanujan <dhruv0122@gmail.com>
2018-06-05 10:19:54 -0400

    Added form submit button functionality

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const button1 = document.querySelector('button');
-const header1 = document.querySelector('#header1');
-const header2 = document.querySelector('#header2');
+const button1 = document.querySelector('button')
+const header1 = document.querySelector('#header1')
+const header2 = document.querySelector('#header2')
 
 
 function changeHeader(){
-    header1.textContent = 'Changed it';
-    header2.textContent = 'Changed it';
+    header2.textContent = 'Changed it'
 
 }
 
+function formSubmit(){
+    const input = document.getElementsByName('change')[0].value;
+    console.log(input)
+    header1.textContent = input
+    return false;
+}
+
 button1.addEventListener('click', changeHeader);
+
