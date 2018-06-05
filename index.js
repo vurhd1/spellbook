5e4538584aef57f6e8e1d3d39e679013de952ce5
Dhruv Ramanujan <dhruv0122@gmail.com>
2018-06-05 09:17:00 -0400

    Added a second header and changed both by pressing the button.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const button1 = document.querySelector('button');
-const header1 = document.querySelector('h1');
+const header1 = document.querySelector('#header1');
+const header2 = document.querySelector('#header2');
+
 
 function changeHeader(){
     header1.textContent = 'Changed it';
+    header2.textContent = 'Changed it';
+
 }
 
 button1.addEventListener('click', changeHeader);
