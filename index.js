0bd1b98af61c8d64f1c42bb43fd077c87b684c9e
Dhruv Ramanujan <dhruv0122@gmail.com>
2018-06-07 10:53:59 -0400

    Refactor code to remove redundency and make small formatting changes

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 const form = document.querySelector('form')
 
+
+const buildSpan = function(name, value, color) {
+    const el = document.createElement('span')
+    el.textContent = value
+    el.classList.add(name)
+    el.style.color = color
+    return el
+}
+
+
 //Changes the first header based on the form input
 const formSubmit = function(ev){
-    ev.preventDefault()
+    ev.preventDefault();
 
     const f = ev.target
     const spell = f.spellName.value
     const mana = f.manaCost.value
-    document.querySelector('#spells').innerHTML +=
-    `<li>
-        <span style="color:red">${spell}</span>
-        \u00A0\u00A0\u00A0 || \u00A0\u00A0\u00A0
-        <span style="color:blue">${mana} mana</span>
-    </li>`
-    //otherAdd(spell, mana)
-}
-
-function otherAdd(spell, mana){
+    
     const list = document.querySelector('ul')
     const item = document.createElement('li')
-    item.appendChild(document.createTextNode(`${spell} \u00A0\u00A0\u00A0 || \u00A0\u00A0\u00A0 ${mana} mana`))
+
+    const spellSpan = buildSpan('spell', spell, 'red')
+    const manaSpan = buildSpan('mana', mana, 'blue')
+
+    item.appendChild(spellSpan)
+    item.appendChild(document.createTextNode(`\u00A0\u00A0\u00A0 || \u00A0\u00A0\u00A0`))
+    item.appendChild(manaSpan)
+    item.classList.add('item')
     list.appendChild(item)
+
 }
 
+
 //Event handler to submit the form
-form.addEventListener('submit', formSubmit)
+form.addEventListener('submit', formSubmit);
 
 //Event handler for pressing enter
 document.addEventListener('keydown', function(event) {
     if(event.keyCode == 13) { //Keycode for enter (13)
-        formSubmit();
+        formSubmit; 
     }
 })
